4b2f5700c17c07d1da34350a1539aba48fb5532c
Ivan Pizarroso <ivnp31@gmail.com>
2022-12-12 20:14:32 +0100

    cambio pass

--- QUIXOTEAPP/config/db.js
+++ QUIXOTEAPP/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
-  password: "Padul23700",
+  password: "belladona",
   port: 3306,
   database: "quixote",
 });
